4f78d558e600031027fe7d3f68b6f7e1a2662010
Ban_1 <93013896+Udoekaobe1@users.noreply.github.com>
2025-06-15 20:25:00 +0100

    third change

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthorsModule],
+  imports: [AuthorsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthorsService } from './authors.service';
+import { UsersService } from './users.service';
 
-describe('AuthorsService', () => {
-  let service: AuthorsService;
+describe('UsersService', () => {
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthorsService],
+      providers: [UsersService],
     }).compile();
 
-    service = module.get<AuthorsService>(AuthorsService);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
